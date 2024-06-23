72d43b91ba1557dd6ef0a445897a9cf95247aae7
“Abdul <kaazees31@gmail.com>
2024-06-23 16:08:26 +0530

    update

--- src/Components/dashboard.js
+++ src/Components/dashboard.js
@@ -33,7 +33,7 @@ const dashboard = () => {
        <div class="d-sm-flex align-items-center justify-content-between mb-4">
         <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
         <a
-          href="#"
+          href="https://example.com"
           class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"
         >
           <i class="fas fa-download fa-sm text-white-50"></i> Generate Report
